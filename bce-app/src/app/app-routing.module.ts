616f3cfc4bf7631d91ac5fc04d32b9530df01440
Mariem Bokri <mariem.bokri@esprit.tn>
2023-11-20 13:16:02 +0100

    details

--- bce-app/src/app/app-routing.module.ts
+++ bce-app/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SeriesSearchComponent } from './series-search/series-search.component';
+import { SeriesDetailsComponent } from './series-details/series-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/search', pathMatch: 'full' },
   { path: 'search', component: SeriesSearchComponent },
+  { path: 'details/:id', component: SeriesDetailsComponent },
 ];
 
 @NgModule({
