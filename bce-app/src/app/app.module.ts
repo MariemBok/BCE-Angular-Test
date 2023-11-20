ca250f65bd26bccf398780a0a1ac5827488a7640
Mariem Bokri <mariem.bokri@esprit.tn>
2023-11-20 09:00:05 +0100

    set up env

--- bce-app/src/app/app.module.ts
+++ bce-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeriesSearchComponent } from './series-search/series-search.component';
+import { SeriesDetailsComponent } from './series-details/series-details.component';
+import { CastDetailsComponent } from './cast-details/cast-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeriesSearchComponent,
+    SeriesDetailsComponent,
+    CastDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ bce-app/src/app/series-search/series-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-series-search',
+  templateUrl: './series-search.component.html',
+  styleUrls: ['./series-search.component.css']
+})
+export class SeriesSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
